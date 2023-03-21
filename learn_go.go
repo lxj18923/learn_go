5cf32aec3b0b24fd445d0bf65adefa1e1723555f
Tobi <lxj18923@163.com>
2023-03-21 15:31:34 +0800

    1.0

---
+++ learn_go.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{3, 5, 1, 6, 8, 4, 2}
+	fmt.Println("Before sorting:", nums)
+	quickSort(nums)
+	fmt.Println("After sorting:", nums)
+}
+
+func quickSort(nums []int) {
+	if len(nums) < 2 {
+		return
+	}
+	pivot := nums[0]
+	left, right := 1, len(nums)-1
+	for left <= right {
+		if nums[left] <= pivot {
+			left++
+		} else {
+			nums[left], nums[right] = nums[right], nums[left]
+			right--
+		}
+	}
+	nums[0], nums[right] = nums[right], nums[0]
+	quickSort(nums[:right])
+	quickSort(nums[right+1:])
+}
